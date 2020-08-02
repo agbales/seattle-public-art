a77ca702b06a2c9419f4db0e4a3d604b8bfbb5d8
Andrew Bales <agbales@gmail.com>
2020-08-02 14:02:16 -0700

    Move js & css to their own files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const data = fetch("https://data.seattle.gov/resource/public-art.json")
+  .then((response) => response.json())
+  .then((data) => {
+    L.mapbox.accessToken =
+      "pk.eyJ1IjoiYWdiYWxlcyIsImEiOiJja2QzaXpoYmcwcHpvMnFwNXFkNGc3bGViIn0.1-It5YQVKRZ1yHxSsuIRAw";
+    var map = L.mapbox
+      .map("map")
+      .setView([47.6062, -122.3321], 13)
+      .addLayer(L.mapbox.styleLayer("mapbox://styles/mapbox/streets-v11"));
+
+    data.forEach((entry) => {
+      const lat = entry.latitude;
+      const long = entry.longitude;
+
+      const artInfo = `<div>
+                      <p>${entry.location}</p>
+                      <p>${entry.title}</P>
+                      <p>${entry.date}</p>
+                      <p>${entry.artist_first_name} ${entry.artist_last_name}</p>
+                    </div>`;
+      L.marker([lat, long]).addTo(map).bindPopup(artInfo);
+    });
+  });
